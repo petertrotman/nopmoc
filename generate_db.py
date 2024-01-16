2a9aabfa973809a00ac7a54aff1ce50e67d197a0
Peter Trotman <petertrotman@gmail.com>
2024-01-16 16:35:51 +0000

    Added footprints to the database

--- generate_db.py
+++ generate_db.py
@@ -3,6 +3,7 @@
 from collections.abc import Iterable
 from itertools import chain
 from kiutils.symbol import SymbolLib, Symbol, SymbolPin
+from kiutils.footprint import Footprint
 from pathlib import Path
 import argparse
 import os
@@ -17,11 +18,43 @@ __location__ = os.path.realpath(
 
 
 def init_db(db: sqlite3.Connection) -> None:
-    """Initialise the database at the given location with
-    the schema defined in schema.sql"""
-    with open(Path(__location__, 'schema.sql')) as f:
-        sql_script = f.read()
-        db.executescript(sql_script)
+    """Initialise the database at the given location with the schema"""
+
+    db.executescript("""
+        CREATE TABLE IF NOT EXISTS
+        symbols(
+            name TEXT,
+            library TEXT
+        );
+        CREATE TABLE IF NOT EXISTS
+        symbol_properties(
+            symbol_rowid references symbols(rowid),
+            key TEXT,
+            value TEXT
+        );
+        CREATE TABLE IF NOT EXISTS
+        symbol_pins(
+            symbol_rowid REFERENCES symbols(rowid),
+            number TEXT,
+            name TEXT,
+            electrical_type TEXT,
+            is_alternate BOOL DEFAULT false
+        );
+        CREATE TABLE IF NOT EXISTS
+        footprints(
+            name TEXT,
+            library TEXT,
+            description TEXT,
+            tags TEXT,
+            pads INTEGER
+        );
+        CREATE TABLE IF NOT EXISTS
+        footprint_properties(
+            footprint_rowid REFERENCES footprints(rowid),
+            key TEXT,
+            value TEXT
+        );
+    """)
 
 
 def insert_library(lib: SymbolLib, db: sqlite3.Connection) -> int:
@@ -40,8 +73,7 @@ def insert_library(lib: SymbolLib, db: sqlite3.Connection) -> int:
 def insert_symbol(
         symbol: Symbol,
         library_name: str,
-        db: sqlite3.Connection
-) -> None:
+        db: sqlite3.Connection):
     """Add the symbol to the db with the given connection
     including all the associated properties and pins"""
 
@@ -57,7 +89,7 @@ def insert_symbol(
     # Insert symbol properties
     cur.executemany(
         """
-        INSERT INTO properties(symbol_rowid, key, value)
+        INSERT INTO symbol_properties(symbol_rowid, key, value)
         VALUES (?, ?, ?);
         """,
         ((symbol_rowid, p.key, p.value) for p in symbol.properties)
@@ -66,7 +98,7 @@ def insert_symbol(
     # Insert pins
     cur.executemany(
         """
-        INSERT INTO pins(
+        INSERT INTO symbol_pins(
             symbol_rowid,
             number,
             name,
@@ -84,7 +116,7 @@ def insert_symbol(
     # Insert alternate pins
     cur.executemany(
         """
-        INSERT INTO pins(
+        INSERT INTO symbol_pins(
             symbol_rowid,
             number,
             name,
@@ -112,6 +144,36 @@ def symbol_pins(symbol: Symbol) -> Iterable[SymbolPin]:
     )
 
 
+def insert_footprint(
+        footprint: Footprint,
+        library_name: str,
+        db: sqlite3.Connection):
+    """Insert the footprint into the database."""
+
+    cur = db.cursor()
+
+    cur.execute("""
+        INSERT INTO footprints(name, library, description, tags, pads)
+        VALUES (?, ?, ?, ?, ?);
+    """, (
+        footprint.entryName,
+        library_name,
+        footprint.description,
+        footprint.tags,
+        len(list(
+            p for p in footprint.pads
+            if isinstance(p.number, int) or len(p.number) > 0
+        ))
+    ))
+
+    footprint_rowid = cur.lastrowid
+
+    cur.executemany("""
+        INSERT INTO footprint_properties(footprint_rowid, key, value)
+        VALUES (?, ?, ?);
+    """, ((footprint_rowid, p.key, p.value) for p in footprint.properties))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="""
         Generate the sqlite database containing data
@@ -130,9 +192,11 @@ if __name__ == '__main__':
     )
 
     args = parser.parse_args()
-    symbols_added = 0
-    lib_paths = list(chain.from_iterable(
+    symbol_library_paths = list(chain.from_iterable(
         Path(p).glob('**/*.kicad_sym') for p in args.search_directories
+    ))
+    footprint_library_paths = list(chain.from_iterable(
+        Path(p).glob('**/*.pretty') for p in args.search_directories
     ))
 
     with sqlite3.connect(args.sqlite_db) as db:
@@ -140,20 +204,28 @@ if __name__ == '__main__':
         init_db(db)
         print('success', end='\n\n')
 
-        for i, library_path in enumerate(lib_paths):
+        for i, slp in enumerate(symbol_library_paths):
             print(
-                f'[{i+1}/{len(lib_paths)}]',
-                f'Processing library {library_path}...',
+                f'[{i+1}/'
+                f'{len(symbol_library_paths) + len(footprint_library_paths)}]',
+                f'Processing symbols from {slp}...',
                 end=' '
             )
-            lib = SymbolLib.from_file(library_path)
-            _symbols_added = insert_library(lib, db)
+            lib = SymbolLib.from_file(slp)
+            symbols_added = insert_library(lib, db)
 
-            print(f'success, {_symbols_added} symbols added')
-            symbols_added += _symbols_added
+            print(f'complete: {symbols_added} symbols added')
 
-    print(
-        "\nSuccess.",
-        f"Inserted {len(lib_paths)} libraries",
-        f"containing {symbols_added} symbols"
-    )
+        for i, flp in enumerate(footprint_library_paths):
+            print(
+                f'[{i+1 + len(symbol_library_paths)}/'
+                f'{len(symbol_library_paths) + len(footprint_library_paths)}]',
+                f'Processing footprints from {flp}...',
+                end=' '
+            )
+            library_name = flp.parts[-1].rsplit('.', 1)[0]
+            footprint_paths = list(flp.glob('**/*.kicad_mod'))
+            for fp in footprint_paths:
+                insert_footprint(Footprint.from_file(fp), library_name, db)
+
+            print(f'complete: {len(footprint_paths)} footprints added')
